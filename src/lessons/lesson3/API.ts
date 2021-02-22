5c81225e0cd818d9f35a6e3d08028b5ed8651ae3
zhankaam <zhankaam@gmail.com>
2021-02-23 01:35:18 +0300

    advanced js

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -3,11 +3,13 @@ import axios from 'axios';
 const configOMB = {
     baseURL: 'http://www.omdbapi.com',
 };
-const key = '';
 const axiosInstance = axios.create(configOMB);
+const key = '?apikey=c0e0eb59';
 
 const API = {
     searchFilmsByTitle: (title: string) => {
+        const query = `${key}&s=${title}`
+        return axiosInstance.get(query)
     },
     searchFilmsByType: (title: string, type: string) => {
     }
--- src/lessons/lesson3/Lesson3.tsx
+++ src/lessons/lesson3/Lesson3.tsx
@@ -4,13 +4,41 @@ import './lesson_3';
 
 const Lesson3 = () => {
     const [searchName, setSearchName] = useState('');
-    const [serachResult, setSerachResult] = useState('');
+    const [searchResult, setSearchResult] = useState('');
     const [searchNameByType, setSearchNameByType] = useState('');
-    const [serachResultByType, setSerachResultByType] = useState('');
+    const [searchResultByType, setSearchResultByType] = useState('');
 
-    const searchFilm = () => {
+    /*const searchFilm = () => {
         API.searchFilmsByTitle(searchName)
-    };
+            .then(({data}) => {
+                if(data.Response === "True"){
+                    setSearchResult(JSON.stringify(data.Search))
+                } else {
+                    setSearchResult(data.Error)
+                }
+            })
+    };*/
+
+
+    /*const searchFilm = async () => {
+     const requestResult = await API.searchFilmsByTitle(searchName)
+        console.log(requestResult)
+        if(requestResult.data.Response === 'True'){
+            setSearchResult(JSON.stringify(requestResult.data.Search))
+        } else {
+            setSearchResult(requestResult.data.Error)
+        }
+    }*/
+
+    const searchFilm = async () => {
+        const {data} = await API.searchFilmsByTitle(searchName)
+        console.log(data)
+        if(data.Response === 'True'){
+            setSearchResult(JSON.stringify(data.Search))
+        } else {
+            setSearchResult(data.Error)
+        }
+    }
 
     const searchByType = (e: React.MouseEvent<HTMLButtonElement>) => {
         const type: string = e.currentTarget.dataset.t ? e.currentTarget.dataset.t : '';
@@ -25,7 +53,7 @@ const Lesson3 = () => {
                 <input type="text" value={searchName} onChange={(e) => setSearchName(e.currentTarget.value)}/>
                 <button onClick={searchFilm}>Search</button>
                 <div>
-                    {serachResult}
+                    {searchResult}
                 </div>
             </div>
 
@@ -35,7 +63,7 @@ const Lesson3 = () => {
                 <button onClick={searchByType} data-t='movie'>Movie</button>
                 <button onClick={searchByType} data-t='series'>Series</button>
                 <div>
-                    {serachResultByType}
+                    {searchResultByType}
                 </div>
             </div>
         </div>
